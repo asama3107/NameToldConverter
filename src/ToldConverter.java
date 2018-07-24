63ef0857c5b688fc372b2d37f4503bb9bd3fec8c
Asama Usuri <asamausuri@MacBook-Pro-Asama.local>
2018-07-24 03:28:17 +0300

    Initial commit

---
+++ src/ToldConverter.java
@@ -0,0 +1,30 @@
+public class ToldConverter {
+
+    public static void main (String args[]){
+
+        int toy = 3;
+
+        switch(toy)
+        {
+            case  0:
+                System.out.println("Car");
+                break;
+            case 1:
+                System.out.println("Lego");
+                break;
+            case 3 :
+                System.out.println("Doll");
+                break;
+            case 4 :
+                System.out.println("Puzzle");
+                break;
+            default :
+                System.out.println("Illegal");
+        }
+
+    }
+}
+
+
+
+
